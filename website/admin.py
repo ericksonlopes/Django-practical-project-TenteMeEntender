f26090416a7290484c7bed24802c577956965e64
EricksonLopes <ofc.erickson@gmail.com>
2020-07-23 11:30:04 -0300

    salvando formulario de contato e mensagem no banco de dados

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sub_title', 'date_post', 'id']
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'id']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Contact, ContactAdmin)
