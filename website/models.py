022f0099e330584c02f0ca0c55f17cb042606ad0
EricksonLopes <ofc.erickson@gmail.com>
2020-07-23 09:38:39 -0300

    create database

--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
+from datetime import date
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    sub_title = models.CharField(max_length=150)
+    text = models.TextField()
+    date_post = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.title
+
